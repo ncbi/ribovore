b0edc042750d4322f1912a1813c0b92da3d158ec
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-07-12 14:13:47 -0400

    Fixes out dir names

--- testfiles/do-all-tests.sh
+++ testfiles/do-all-tests.sh
@@ -1,17 +1,17 @@
 # non-parallel 16 sequence test
-$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.example-16 test-16
+$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.example-16 test1
 # parallel 16 sequence test
-$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.p.example-16 test-p-16
+$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.p.example-16 test2
 
 # non-parallel 100 sequence test
-$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.r100 test-100
+$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.r100 test3
 # parallel 100 sequence test
-$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.p.r100 test-p-100
+$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.p.r100 test4
 
 # non-parallel ribodbcreate.pl test
-$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.p.db test-db 
+$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.p.db test5 
 # parallel ribodbcreate.pl test
-$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.p.db test-p-db
+$RIBODIR/ribotest.pl -f $RIBODIR/testfiles/testin.p.db test6
 
 # optionally remove all test directories
 #for d in test-16 test-p-16 test-100 test-p-100 test-db test-p-db; do 
