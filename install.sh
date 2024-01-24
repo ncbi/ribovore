d0b6f3a0dfd13e55304c38f9b7e0e3b9ed8416db
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2024-01-24 12:18:08 -0500

    Adds blast install to ribotyper-only installs

--- install.sh
+++ install.sh
@@ -262,20 +262,20 @@ fi
 ################################
     
 # download blast binaries
+if [ "$INPUTSYSTEM" = "linux" ]; then
+    echo "Downloading BLAST version $BVERSION for Linux"
+    curl -k -L -o blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/$BVERSION/ncbi-blast-$BVERSION+-x64-linux.tar.gz
+else 
+    echo "Downloading BLAST version $BVERSION for Mac/OSX"
+    echo "curl -k -L -o blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/$BVERSION/ncbi-blast-$BVERSION+-x64-macosx.tar.gz"
+    curl -k -L -o blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/$BVERSION/ncbi-blast-$BVERSION+-x64-macosx.tar.gz
+fi
+tar xfz blast.tar.gz
+rm blast.tar.gz
+mv ncbi-blast-$BVERSION+ ncbi-blast
+echo "------------------------------------------------"
+
 if [ "$RIBOTYPERONLY" != "yes" ]; then
-    if [ "$INPUTSYSTEM" = "linux" ]; then
-        echo "Downloading BLAST version $BVERSION for Linux"
-        curl -k -L -o blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/$BVERSION/ncbi-blast-$BVERSION+-x64-linux.tar.gz
-    else 
-        echo "Downloading BLAST version $BVERSION for Mac/OSX"
-        echo "curl -k -L -o blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/$BVERSION/ncbi-blast-$BVERSION+-x64-macosx.tar.gz"
-        curl -k -L -o blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/$BVERSION/ncbi-blast-$BVERSION+-x64-macosx.tar.gz
-    fi
-    tar xfz blast.tar.gz
-    rm blast.tar.gz
-    mv ncbi-blast-$BVERSION+ ncbi-blast
-    echo "------------------------------------------------"
-    
     # if linux, download vecscreen_plus_taxonomy
     if [ "$INPUTSYSTEM" = "linux" ]; then
         curl -k -L -o vecscreen_plus_taxonomy-$RVERSION.zip https://github.com/aaschaffer/vecscreen_plus_taxonomy/archive/$RVERSION.zip; 
@@ -300,15 +300,13 @@ if [ "$DOKEEP" = "no" ]; then
     mkdir infernal
     mv binaries infernal
     # ncbi-blast, we only need blastdbcmd and blastn
-    if [ "$RIBOTYPERONLY" != "yes" ]; then
-        mv ncbi-blast/bin/blastdbcmd ./
-        mv ncbi-blast/bin/blastn ./
-        rm -rf ncbi-blast
-        mkdir ncbi-blast
-        mkdir ncbi-blast/bin
-        mv blastdbcmd ncbi-blast/bin
-        mv blastn ncbi-blast/bin
-    fi
+    mv ncbi-blast/bin/blastdbcmd ./
+    mv ncbi-blast/bin/blastn ./
+    rm -rf ncbi-blast
+    mkdir ncbi-blast
+    mkdir ncbi-blast/bin
+    mv blastdbcmd ncbi-blast/bin
+    mv blastn ncbi-blast/bin
     echo "------------------------------------------------"
 fi
 ########################################################
@@ -330,8 +328,8 @@ echo "export RIBOINFERNALDIR=\"\$RIBOINSTALLDIR/infernal/binaries\""
 echo "export RIBOEASELDIR=\"\$RIBOINSTALLDIR/infernal/binaries\""
 echo "export RIBOSEQUIPDIR=\"\$RIBOINSTALLDIR/sequip\""
 echo "export RIBOTIMEDIR=\"/usr/bin\""
+echo "export RIBOBLASTDIR=\"\$RIBOINSTALLDIR/ncbi-blast/bin\""
 if [ "$RIBOTYPERONLY" != "yes" ]; then
-    echo "export RIBOBLASTDIR=\"\$RIBOINSTALLDIR/ncbi-blast/bin\""
     echo "export RRNASENSORDIR=\"\$RIBOINSTALLDIR/rRNA_sensor\""
     if [ "$INPUTSYSTEM" = "linux" ]; then
         echo "export VECPLUSDIR=\"\$RIBOINSTALLDIR/vecscreen_plus_taxonomy\""
